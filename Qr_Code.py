37c86ff0bdd7997d6e72fd0c715b8027effd4683
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-09-08 10:51:30 +0530

    Updating Commit
    Comments were added and extra set of unwanted code was removed which was added mistakely because of issues

--- Qr_Code.py
+++ Qr_Code.py
@@ -1,9 +1,16 @@
+# From tkinter import. Scale
 from tkinter import Scale
+# import png
 import png
-from tkinter import Scale
+# Import pyqrcode.
 import pyqrcode
+# Deserialize from PIL import image
 from PIL import Image
+# Enter the link to convert it to QR code
 link = input("Enter the link to convert it to QR code : ")
+# Create a qrcode link.
 qr_code = pyqrcode.create(link)
+# QRCode. png QRCode
 qr_code.png ("QRCode.png", scale=5)
+# QRCode. png
 Image.open = ("QRCode.png")
